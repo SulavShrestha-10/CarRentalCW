d47b05e15139fbf4da4b10166972b69636e74a10
Sulav Shrestha <sulavstha2629@gmail.com>
2023-04-30 15:27:18 +0545

    Added Image Upload After Registration

---
+++ Models/AddImagesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CarRentalApp.Models
+{
+    public class AddImagesViewModel
+    {
+        [Required(ErrorMessage = "Please select a citizenship image.")]
+        [Display(Name = "Citizenship Image")]
+        public IFormFile CitizenshipImage { get; set; }
+
+        [Required(ErrorMessage = "Please select a driving license image.")]
+        [Display(Name = "Driving License Image")]
+        public IFormFile DrivingLicenseImage { get; set; }
+        public bool CitizenshipImageExists { get; set; }
+        public bool DrivingLicenseImageExists { get; set; }
+    }
+}
